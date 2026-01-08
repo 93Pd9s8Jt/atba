5792ec2046f7d5c7f3feaa4cb979bc91eabff182
93Pd9s8Jt <92815501+93Pd9s8Jt@users.noreply.github.com>
2026-01-08 14:18:42 +0000

    chore: tidy up database name

--- lib/services/cache/web.dart
+++ lib/services/cache/web.dart
@@ -1,21 +1,24 @@
-
 import 'package:drift/drift.dart';
 import 'package:drift/wasm.dart';
 import 'package:flutter/foundation.dart';
 
 DatabaseConnection connect() {
-  return DatabaseConnection.delayed(Future(() async {
-    final db = await WasmDatabase.open(
-      databaseName: 'todo-app',
-      sqlite3Uri: Uri.parse('sqlite3.wasm'),
-      driftWorkerUri: Uri.parse('drift_worker.js'),
-    );
+  return DatabaseConnection.delayed(
+    Future(() async {
+      final db = await WasmDatabase.open(
+        databaseName: 'torbox_cache',
+        sqlite3Uri: Uri.parse('sqlite3.wasm'),
+        driftWorkerUri: Uri.parse('drift_worker.js'),
+      );
 
-    if (db.missingFeatures.isNotEmpty) {
-      debugPrint('Using ${db.chosenImplementation} due to unsupported '
-          'browser features: ${db.missingFeatures}');
-    }
+      if (db.missingFeatures.isNotEmpty) {
+        debugPrint(
+          'Using ${db.chosenImplementation} due to unsupported '
+          'browser features: ${db.missingFeatures}',
+        );
+      }
 
-    return db.resolvedExecutor;
-  }));
+      return db.resolvedExecutor;
+    }),
+  );
 }
